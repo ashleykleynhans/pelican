65b5580e6f1a5cc5e087284711ffe0a9026caced
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-01-19 00:06:41 +0200

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pelican content -o output -s pelicanconf.py
 ghp-import output -b gh-pages
-git push git@github.com:ashleykleynhans/ashleykleynhans.github.io.git gh-pages:master
+git push git@github.com:ashleykleynhans/ashleykleynhans.github.io.git gh-pages:master --force
