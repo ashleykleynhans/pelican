3409cb93f01cbe2a99ec2f7a9ef255bda549f42b
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-01-18 22:14:23 +0200

    Styling and different pgyments style

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,26 +2,27 @@ from datetime import datetime
 
 AUTHOR = 'Ashley Kleynhans'
 SITEURL = ''
-SITENAME = "Ashley Kleynhans"
+SITENAME = "Ashley's Blog"
 SITETITLE = 'Ashley Kleynhans'
 SITESUBTITLE = 'DevOps Engineer'
-BROWSER_COLOR = "#333333"
+BROWSER_COLOR = '#333333'
 SITELOGO = SITEURL + '/images/profile.jpeg'
 FAVICON = SITEURL + '/images/favicon.ico'
 
 # Code highlighting the theme
-PYGMENTS_STYLE = "monokai"
+PYGMENTS_STYLE = 'solarized-dark'
 
-ROBOTS = "index, follow"
+ROBOTS = 'index, follow'
 
 PATH = 'content'
-OUTPUT_PATH = "blog/"
+OUTPUT_PATH = 'blog/'
 TIMEZONE = 'Africa/Johannesburg'
 
 DISABLE_URL_HASH = True
 
 DEFAULT_LANG = 'en'
 THEME = 'themes/flex'
+CUSTOM_CSS = 'extra/custom.css'
 
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -33,7 +34,7 @@ USE_FOLDER_AS_CATEGORY = False
 MAIN_MENU = True
 HOME_HIDE_TAGS = True
 
-GITHUB_CORNER_URL = "https://github.com/ashleykleynhans/pelican"
+GITHUB_CORNER_URL = 'https://github.com/ashleykleynhans/pelican'
 
 SOCIAL = (
     ('github', 'https://github.com/ashleykleynhans'),
@@ -46,31 +47,39 @@ MENUITEMS = (
     ('Tags', '/tags')
 )
 
+LINKS = (
+    ('Home', '/'),
+)
+
 CC_LICENSE = {
-    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
-    "version": "4.0",
-    "slug": "by-sa",
-    "icon": True,
-    "language": "en_US",
+    'name': 'Creative Commons Attribution-ShareAlike 4.0 International License',
+    'version': '4.0',
+    'slug': 'by-sa',
+    'icon': True,
+    'language': 'en_US',
 }
 
 COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['images', 'extra']
+STATIC_PATHS = [
+    'images',
+    'extra'
+]
 
 THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
 THEME_COLOR_ENABLE_USER_OVERRIDE = True
 
 USE_LESS = True
 
-PLUGIN_PATHS = ['./pelican-plugins']
+PLUGIN_PATHS = [
+    './pelican-plugins'
+]
 
 PLUGINS = [
     'sitemap',
     'post_stats'
 ]
-
 
 # Sitemap Settings
 SITEMAP = {
