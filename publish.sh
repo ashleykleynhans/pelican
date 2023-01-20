3a672e5ef3c3c2a69f86248e46299604ab4c7476
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-01-20 10:46:24 +0200

    Use publishconf.py for publishing to fix sitemap

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pelican content -o output -s pelicanconf.py
+pelican content -o output -s publishconf.py
 ghp-import output -b gh-pages
 git push git@github.com:ashleykleynhans/ashleykleynhans.github.io.git gh-pages:master --force
