9a65cbbb06587fe8b01929df64a7523b9f40a981
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2026-03-19 12:28:50 +0200

    Fix publish.sh paths, update console URLs, and add target=_blank to links

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-/Users/ashley/src/personal/blog/.venv/bin/pelican content -o output -s publishconf.py
+.venv/bin/pelican content -o output -s publishconf.py
 
 # This can remove your new articles so add them in first
 cp index.html output/index.html
 
-ghp-import output -b gh-pages
+.venv/bin/ghp-import output -b gh-pages
 git push git@github.com:ashleykleynhans/ashleykleynhans.github.io.git gh-pages:master --force
